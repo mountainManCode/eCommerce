3c6ded1eaccc1712ba66c2ed5fdc433a54d90d41
Aaron <aa.griff9@gmail.com>
2018-10-16 14:07:02 -0700

    Styling and added _document to render styles on server side.

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,11 +1,23 @@
-import Nav from './Nav';
 import Link from 'next/link';
 import styled from 'styled-components';
+import Nav from './Nav';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+
+Router.onRouteChangeStart = () => {
+  NProgress.start();
+};
+Router.onRouteChangeComplete = () => {
+  NProgress.done();
+};
+Router.onRouteChangeError = () => {
+  NProgress.done();
+};
 
 const Logo = styled.h1`
   font-size: 4rem;
   margin-left: 2rem;
-  position: relative:
+  position: relative;
   z-index: 2;
   transform: skew(-7deg);
   a {
@@ -16,7 +28,7 @@ const Logo = styled.h1`
     text-decoration: none;
   }
   @media (max-width: 1300px) {
-    margin: 0,
+    margin: 0;
     text-align: center;
   }
 `;
@@ -32,13 +44,14 @@ const StyledHeader = styled.header`
       grid-template-columns: 1fr;
       justify-content: center;
     }
-    .sub-bar {
-      display: grid;
-      grid-template-columns: 1fr auto;
-      border-bottom: 1px solid ${props => props.theme.lightgrey};
-    }
+  }
+  .sub-bar {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    border-bottom: 1px solid ${props => props.theme.lightgrey};
   }
 `;
+
 const Header = () => (
   <StyledHeader>
     <div className="bar">
@@ -53,7 +66,9 @@ const Header = () => (
     <div className="sub-bar">
       <p>Search</p>
     </div>
-    <div>Cart</div>
+    <div>
+      <p>Cart</p>
+    </div>
   </StyledHeader>
 );
 export default Header;
--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
-import Header from '../components/Header';
-import Meta from '../components/Meta';
 import styled, { ThemeProvider, injectGlobal } from 'styled-components';
+import Header from './Header';
+import Meta from './Meta';
 
 const theme = {
   red: '#FF0000',
   black: '#393939',
   grey: '#3A3A3A',
-  lightgrey: 'E1E1E1',
+  lightgrey: '#E1E1E1',
   offWhite: '#EDEDED',
   maxWidth: '1000px',
-  bs: '0 12px 24px 0 rgba(0, 0, 0.09)'
+  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)'
 };
 
 const StyledPage = styled.div`
@@ -22,6 +22,34 @@ const Inner = styled.div`
   max-width: ${props => props.theme.maxWidth};
   margin: 0 auto;
   padding: 2rem;
+`;
+
+injectGlobal`
+  @font-face {
+    font-family: 'radnika_next';
+    src: url('/static/radnikanext-medium-webfont.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
+  html {
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+    font-family: 'radnika_next';
+  }
+  a {
+    text-decoration: none;
+    color: ${theme.black};
+  }
+  button {  font-family: 'radnika_next'; }
 `;
 
 class Page extends Component {
