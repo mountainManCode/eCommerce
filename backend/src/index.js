23b40e45360f14fc248bd32c52ce62084ebe9d15
Aaron <aa.griff9@gmail.com>
2018-10-22 17:22:16 -0700

    Setting up backend schemas, Yoga and Prisma server, Query and Mutations are working.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,23 @@
-// let's go!
+require('dotenv').config({ path: 'variables.env' });
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+// Express middleware to handle cookies (JWT)
+
+// Express middleware to populate current user
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    }
+  },
+  deets => {
+    console.log(
+      `Server is now running on port https://localhost:${deets.port}`
+    );
+  }
+);
--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,26 @@
-const mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: check if they are logged in
 
-module.exports = mutations;
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
+
+    console.log(item);
+
+    return item;
+  }
+  // createDog(parent, args, ctx, info) {
+  //   global.dogs = global.dogs || [];
+  //   const newDog = { name: args.name };
+  //   global.dogs.push(newDog);
+  //   return newDog;
+  // }
+};
+
+module.exports = Mutations;
