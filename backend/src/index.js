f50f19a8ed892771dcd687d6e25863815a78a2f3
Aaron <aa.griff9@gmail.com>
2018-10-25 23:36:13 -0700

    Grabbing data from the json web token.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,6 @@
 const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
 const db = require('./db');
@@ -7,6 +9,17 @@ const server = createServer();
 
 // Express middleware to handle cookies (JWT)
 server.express.use(cookieParser());
+
+// decode the JWT so we can get the user ID on each request.
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    // put the userId onto the req for future requests to access.
+    req.userId = userId;
+  }
+  next();
+});
 
 // Express middleware to populate current user
 
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -3,13 +3,20 @@ const { forwardTo } = require('prisma-binding');
 const Query = {
   items: forwardTo('db'),
   item: forwardTo('db'),
-  itemsConnection: forwardTo('db')
-
-  // async items(parent, args, ctx, info) {
-  //   const items = await ctx.db.query.items();
-
-  //   return items;
-  // }
+  itemsConnection: forwardTo('db'),
+  me(parent, args, ctx, info) {
+    // check if there isa current userId
+    // GOTCHA: Be sure to spell out 'request', do not shorten it to req
+    if (!ctx.request.userId) {
+      return null;
+    }
+    return ctx.db.query.user(
+      {
+        where: { id: ctx.request.userId }
+      },
+      info
+    );
+  }
 };
 
 module.exports = Query;
