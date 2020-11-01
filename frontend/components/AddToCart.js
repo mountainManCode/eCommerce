e435f53bc8ce65f51a8664089fd35faa9b4abcfe
Aaron <aa.griff9@gmail.com>
2020-11-01 00:10:15 -0700

    Displaying cart items and totals.

--- frontend/components/AddToCart.js
+++ frontend/components/AddToCart.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
+import { CURRENT_USER_QUERY } from './User';
 
 const ADD_TO_CART_MUTATION = gql`
 	mutation addToCart($id: ID!) {
@@ -13,10 +14,18 @@ const ADD_TO_CART_MUTATION = gql`
 
 class AddToCart extends React.Component {
 	render() {
-		const {id} = this.props;
+		const { id } = this.props;
 		return (
-			<Mutation mutation={ADD_TO_CART_MUTATION} variables={{id}}>
-				{addToCart => <button onClick={addToCart}>Scooby To Cart</button>}
+			<Mutation
+				mutation={ ADD_TO_CART_MUTATION }
+				variables={ { id } }
+				refetchQueries={ [{ query: CURRENT_USER_QUERY }] }
+			>
+				{ (addToCart, { loading }) =>
+					<button disabled={ loading } onClick={ addToCart }>
+						Add{ loading && 'ing' } To Cart
+					</button>
+				}
 			</Mutation>
 		)
 	}
--- frontend/components/User.js
+++ frontend/components/User.js
@@ -3,24 +3,35 @@ import gql from 'graphql-tag';
 import PropTypes from 'prop-types';
 
 const CURRENT_USER_QUERY = gql`
-  query {
-    me {
-      id
-      email
-      name
-      permissions
-    }
-  }
+	query {
+		me {
+			id
+			email
+			name
+			permissions
+			cart{
+				id
+				quantity
+				item {
+					id
+					price
+					image
+					title
+					description
+				}
+			}
+		}
+	}
 `;
 
 const User = props => (
-  <Query {...props} query={CURRENT_USER_QUERY}>
-    {payload => props.children(payload)}
-  </Query>
+	<Query {...props} query={CURRENT_USER_QUERY}>
+		{payload => props.children(payload)}
+	</Query>
 );
 
 User.propTypes = {
-  children: PropTypes.func.isRequired
+	children: PropTypes.func.isRequired
 };
 export default User;
 export { CURRENT_USER_QUERY };
