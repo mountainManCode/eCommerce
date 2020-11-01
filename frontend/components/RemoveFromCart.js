6525732cf9072065a40d776fa5209108e26e98d4
Aaron <aa.griff9@gmail.com>
2020-11-01 15:25:00 -0800

    Deleting Items from cart on Frontend with optimisticResponse.

--- frontend/components/RemoveFromCart.js
+++ frontend/components/RemoveFromCart.js
@@ -28,10 +28,33 @@ class RemoveFromCart extends React.Component {
 		id: PropTypes.string.isRequired,
 	};
 
+	// This gets called as soon as we get a Response back from the server after a mutation has been performed.
+	updateCartCache = (cache, payload) => {
+		// 1. Read the cache.
+		const data = cache.readQuery({ query: CURRENT_USER_QUERY });
+		console.log('DATA', data);
+		// 2. Remove the item from the cart.
+		const cartItemId = payload.data.removeFromCart.id;
+		data.me.cart = data.me.cart.filter(cartItem => cartItem.id !== cartItemId);
+		// 3. write it back to the cache.
+		cache.writeQuery({ query: CURRENT_USER_QUERY, data });
+	};
+
 	render() {
 		return (
 
-			<Mutation mutation={ REMOVE_FROM_CART_MUTATION } variables={ { id: this.props.id } }>
+			<Mutation
+				mutation={ REMOVE_FROM_CART_MUTATION }
+				variables={ { id: this.props.id } }
+				update={ this.updateCartCache }
+				optimisticResponse={ {
+					__typename: 'Mutation',
+					removeFromCart: {
+						__typename: 'CartItem',
+						id: this.props.id,
+					},
+				} }
+			>
 				{ (removeFromCart, { error, loading }) => (
 					<BigButton
 						disabled={ loading }
