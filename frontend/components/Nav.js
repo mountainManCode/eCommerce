2e32f3de7f9e48cace10da0616ad9e9e88722382
Aaron <aa.griff9@gmail.com>
2020-11-01 16:07:01 -0800

    Added a Cart Counter on the Nav Bar.

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,38 +1,42 @@
 import Link from 'next/link';
 import { Mutation } from 'react-apollo';
 import { TOGGLE_CART_MUTATION } from './Cart';
-import NavStyles from './styles/NavStyles';
+import CartCount from './CartCount';
 import User from './User';
 import Signout from '../components/Signout';
+import NavStyles from './styles/NavStyles';
 
 const Nav = () => (
   <User>
-    {({ data: { me } }) => (
+    { ({ data: { me } }) => (
       <NavStyles>
-      { me && (
-        <p>{ me.name }</p>
-      )}
+        { me && (
+          <p>{ me.name }</p>
+        ) }
         <Link href="/items">
           <a>Shop</a>
         </Link>
 
         { me && (
           <>
-            <Link href="/sell">
-              <a>Sell</a>
-            </Link>
-            <Link href="/orders">
-              <a>Orders</a>
-            </Link>
-            <Link href="/me">
-              <a>Account</a>
-            </Link>
-            <Signout />
-            <Mutation mutation={TOGGLE_CART_MUTATION}>
-              { toggleCart => (
-                <button onClick={ toggleCart }>My Cart</button>
-              )}
-            </Mutation>
+          <Link href="/sell">
+            <a>Sell</a>
+          </Link>
+          <Link href="/orders">
+            <a>Orders</a>
+          </Link>
+          <Link href="/me">
+            <a>Account</a>
+          </Link>
+          <Signout />
+          <Mutation mutation={ TOGGLE_CART_MUTATION }>
+            { toggleCart => (
+              <button onClick={ toggleCart }>
+                Cart
+                <CartCount count={ me.cart.reduce((tally, cartItem) => tally + cartItem.quantity, 0) }></CartCount>
+              </button>
+            ) }
+          </Mutation>
           </>
         )}
 
@@ -40,9 +44,9 @@ const Nav = () => (
           <Link href="/signup">
             <a>Sign In</a>
           </Link>
-        )}
+        ) }
       </NavStyles>
-    )}
+    ) }
   </User>
 );
 
